4d6cf403a47aebf6b69bc2b6d0405496aa7a441d
johann-gorban <ivangorbanwork@gmail.com>
2025-04-11 18:22:47 +0300

    Node noexcept keyword added

--- node.hpp
+++ node.hpp
@@ -11,50 +11,50 @@ public:
     Node *left;
     Node *right;
 public:
-    Node();
+    Node() noexcept;
 
-    Node(TYPE);
+    Node(TYPE) noexcept;
 
     ~Node() = default;
 public:
-    static int get_height(Node<TYPE> *);
+    static int get_height(Node<TYPE> *) noexcept;
 
-    static int get_balance(Node<TYPE> *);
+    static int get_balance(Node<TYPE> *) noexcept;
 
-    static void update_height(Node<TYPE> *);
+    static void update_height(Node<TYPE> *) noexcept;
 
-    static Node<TYPE> *rotate_right(Node<TYPE> *);
+    static Node<TYPE> *rotate_right(Node<TYPE> *) noexcept;
     
-    static Node<TYPE> *rotate_left(Node<TYPE> *);
+    static Node<TYPE> *rotate_left(Node<TYPE> *) noexcept;
 
-    static Node<TYPE> *balance(Node<TYPE> *);
+    static Node<TYPE> *balance(Node<TYPE> *) noexcept;
 };
 
 template<typename TYPE>
-Node<TYPE>::Node() : data(TYPE{}), height(1), left(nullptr), right(nullptr) {}
+Node<TYPE>::Node() noexcept : data(TYPE{}), height(1), left(nullptr), right(nullptr) {}
 
 template<typename TYPE>
-Node<TYPE>::Node(TYPE value) : data(value), height(1), left(nullptr), right(nullptr) {}
+Node<TYPE>::Node(TYPE value) noexcept : data(value), height(1), left(nullptr), right(nullptr) {}
 
 template<typename TYPE>
-int Node<TYPE>::get_height(Node<TYPE> *node) {
+int Node<TYPE>::get_height(Node<TYPE> *node) noexcept {
     return node ? node->height : 0;
 }
 
 template<typename TYPE>
-int Node<TYPE>::get_balance(Node<TYPE> *node) {
+int Node<TYPE>::get_balance(Node<TYPE> *node) noexcept {
     return node ? Node<TYPE>::get_height(node->left) - Node<TYPE>::get_height(node->right) : 0;
 }   
 
 template<typename TYPE>
-void Node<TYPE>::update_height(Node<TYPE> *node) {
+void Node<TYPE>::update_height(Node<TYPE> *node) noexcept {
     if (node) {
         node->height = 1 + std::max(Node<TYPE>::get_height(node->left), Node<TYPE>::get_height(node->right));
     }
 }
 
 template<typename TYPE>
-Node<TYPE> *Node<TYPE>::rotate_right(Node<TYPE> *y) {
+Node<TYPE> *Node<TYPE>::rotate_right(Node<TYPE> *y) noexcept {
     Node<TYPE> *x = y->left;
     Node<TYPE> *T2 = x->right;
 
@@ -68,7 +68,7 @@ Node<TYPE> *Node<TYPE>::rotate_right(Node<TYPE> *y) {
 }
 
 template<typename TYPE>
-Node<TYPE> *Node<TYPE>::rotate_left(Node<TYPE> *x) {
+Node<TYPE> *Node<TYPE>::rotate_left(Node<TYPE> *x) noexcept {
     Node<TYPE> *y = x->right;
     Node<TYPE> *T2 = y->left;
 
@@ -82,7 +82,7 @@ Node<TYPE> *Node<TYPE>::rotate_left(Node<TYPE> *x) {
 }
 
 template<typename TYPE>
-Node<TYPE>* Node<TYPE>::balance(Node<TYPE>* node) {
+Node<TYPE>* Node<TYPE>::balance(Node<TYPE>* node) noexcept {
     Node<TYPE>::update_height(node);
     int balance_factor = Node<TYPE>::get_balance(node);
 
